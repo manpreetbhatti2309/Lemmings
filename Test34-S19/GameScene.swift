bf9a3643156dd4f5e98ae15c8f264c5483be2c95
manpreet bhatti <manubhatti2309@gmail.com>
2019-06-19 13:19:24 -0400

    making of mario spawn added mouse click event collision with mario and platform

--- Test34-S19/GameScene.swift
+++ Test34-S19/GameScene.swift
@@ -13,16 +13,66 @@ class GameScene: SKScene {
     
     var nextLevelButton:SKLabelNode!
     
+ 
+    let mario = SKSpriteNode(imageNamed: "mario")
+    //let square = SKSpriteNode(imageNamed: "square")
+    //let triangle = SKSpriteNode(imageNamed: "triangle")
+    //let lshape = SKSpriteNode(imageNamed:"L")
+    
+    var timeOfLastUpdate:TimeInterval = 0
+    var dt: TimeInterval = 0
+    
+    
+    func spawnmario() {
+        let mario = SKSpriteNode(imageNamed:"mario")
+        
+        // put sand at a random (x,y) position
+        let x = self.size.width/2
+        let y = self.size.height - 100
+        mario.position.x = x
+        mario.position.y = y
+        
+        // add physics
+        mario.physicsBody = SKPhysicsBody(circleOfRadius: mario.size.width / 2)
+        self.mario.physicsBody?.affectedByGravity = true
+        
+        addChild(mario)
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // make new sand every 10ms
+        self.dt = currentTime - timeOfLastUpdate
+        if (self.dt >= 0.9) {
+            timeOfLastUpdate = currentTime
+            self.spawnmario()
+        }
+       
+        
+        
+    }
+    
     
     override func didMove(to view: SKView) {
         print("This is level 1")
-        self.nextLevelButton = self.childNode(withName: "nextLevelButton") as! SKLabelNode
+ self.nextLevelButton = self.childNode(withName: "nextLevelButton") as! SKLabelNode
+       
+        
+    }
+    func makePlatform(xPosition:CGFloat, yPosition:CGFloat) {
+        
+        // 1. create an orange sprite
+        let platform = SKSpriteNode(imageNamed: "platform")
+        
+        // 2. set initial position of orange to be same
+        // as where mouse is clicked
+        platform.position.x = xPosition;
+        platform.position.y = yPosition;
+        
+        addChild(platform)
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-    // touch
+    var mouseStartingPosition:CGPoint = CGPoint(x:0, y:0)
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         if (touch == nil) {
@@ -46,6 +96,22 @@ class GameScene: SKScene {
                 scene!.scaleMode = .aspectFill
                 view?.presentScene(scene!)
             }
+        }
+        
+        // 1. detect where the person clicked
+        let touches = touches.first!
+        let mousePosition = touches.location(in:self)
+        
+        let spriteTouched = self.atPoint(mousePosition)
+        if (spriteTouched.name == "platform") {
+//            self.makePlatform(
+//                xPosition:mousePosition.x,
+//                yPosition:mousePosition.y)
+            self.mouseStartingPosition = mousePosition
+            
+            
+            print("Starting position: \(mouseStartingPosition.x), \(mouseStartingPosition.y)")
+            print("Ending position: \(mousePosition.x), \(mousePosition.y)")
         }
         
     }
